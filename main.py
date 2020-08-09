9fe23fc1c43c2f2feeefe3a6109dbcd89b89367a
BCanary <mike_channel2@mail.ru>
2020-08-09 15:35:09 +0300

    fix, url now getting from mpt ru, not just an const

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import xlrd
 import telebot
 from telebot import types
+from bs4 import BeautifulSoup
 
 markup = types.ReplyKeyboardMarkup()
 itembtn1 = types.KeyboardButton('информация')
@@ -12,16 +13,36 @@ token = "1364150425:AAErYiwGtWHTM11dL82aY6uL4rte6IwhqiM"
 
 bot = telebot.TeleBot(token)
 
-XLSX_URL = "https://6c869467-a-7da13995-s-sites.googlegroups.com/a/mpt.ru/priemnaa-komissia-2020/home/090207%20%20%20%D0%91%D0%AE%D0%94%D0%96%D0%95%D0%A2%20.xlsx?attachauth=ANoY7coOmUFB5Ramu3cJrEdrMzRqXq0HTbQgyRTUsY6cc-4ryf5yMUOiLqyNTpBCi_qSygzWeaXNStBDBnpzgpUEejaajauLUfKA1-WBOUse91A5CO7lx1lQ17_3HVpDbcLte1zps7i6hwSAwtd0rdnju4Z4pX7INVjAKnoEEF_yTpCL99L2owONfUNV3-oA_EaqByXfp9VsC-HmmsTOnDQ5gcFtJf5rkppU-UZmA4fQSDIXsz6rkN8wi_ZlVbXkrEvc9oyu8UCEon0tqbBDHVCuGWYnCtOdsg%3D%3D&attredirects=0&d=1"
+#XLSX_URL = "https://6c869467-a-7da13995-s-sites.googlegroups.com/a/mpt.ru/priemnaa-komissia-2020/home/090207%20%20%20%D0%91%D0%AE%D0%94%D0%96%D0%95%D0%A2%20.xlsx?attachauth=ANoY7coOmUFB5Ramu3cJrEdrMzRqXq0HTbQgyRTUsY6cc-4ryf5yMUOiLqyNTpBCi_qSygzWeaXNStBDBnpzgpUEejaajauLUfKA1-WBOUse91A5CO7lx1lQ17_3HVpDbcLte1zps7i6hwSAwtd0rdnju4Z4pX7INVjAKnoEEF_yTpCL99L2owONfUNV3-oA_EaqByXfp9VsC-HmmsTOnDQ5gcFtJf5rkppU-UZmA4fQSDIXsz6rkN8wi_ZlVbXkrEvc9oyu8UCEon0tqbBDHVCuGWYnCtOdsg%3D%3D&attredirects=0&d=1"
 excel_file = xlrd.open_workbook('temp.xlsx')
 
 #выбираем активный лист
 sheet = excel_file.sheet_by_index(0)
 
 def getExcelTable():
-	global XLSX_URL, excel_file, sheet
+	global excel_file, sheet
+	
+	r = requests.get("https://sites.google.com/a/mpt.ru/priemnaa-komissia-2020/")
+	
+	print(r.status_code)
+	
+	b = BeautifulSoup(r.content, "lxml")
+	
+	
+	r = requests.get(b.find_all("table")[3].find_all("tr")[2].find_all("td")[1].find("a")["href"])
+	
+	text_start = r.text.find("http://sites.google.com/a/mpt.ru/priemnaa-komissia-2020/home/")
+	
+	text_end = text_start
+	while True:
+		text_end += 1
+		if r.text[text_end] == "\"":
+			break 
+	
+	xls_url = r.text[text_start:text_end]
+	
 	# download the file contents in binary format
-	r = requests.get(XLSX_URL)
+	r = requests.get(xls_url)
 	 
 	# open method to open a file on your system and write the contents
 	with open("temp.xlsx", "wb") as code:
@@ -69,13 +90,12 @@ def getExcelPosition():
 def get_text_messages(message):
 	global getExcelTable
 	if (message.text == "/start"):
-		bot.send_message(message.from_user.id, "Жми кнопку ЖМИ КНОПКУ!!!", reply_markup=markup)
+		bot.send_message(message.from_user.id, "Жми кнопку", reply_markup=markup)
 	if (message.text) == "информация":
 		bot.send_message(message.from_user.id, "Отправил запрос на удаленный сервер... Жду файла xlsx")
 		getExcelTable()
 		response = getExcelPosition()
 		bot.send_message(message.from_user.id, "Ты на: " + response["position"] + " месте\nВсего подано уведомлений: " + response["uvedomleni"] + "\n" + "Всего подано заявлений:" + response["vsego"] + "\nОбщий средний балл:" + response["sredni"], reply_markup=markup)
-
 
 #print(getExcelPosition()["sredni"])
 bot.polling(none_stop=True, interval=0)
