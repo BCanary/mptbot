e027daa52150196a22a4a4ea9fa132a5e048540f
BCanary <mike_channel2@mail.ru>
2020-08-13 20:51:41 +0300

    Now in VK

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # import the requests library
 import requests
 import xlrd
-import telebot
+import vk_api
+from random import randint
+from vk_api.longpoll import VkLongPoll, VkEventType
 from telebot import types
 from bs4 import BeautifulSoup
 
@@ -9,13 +11,11 @@ from bs4 import BeautifulSoup
 ОСТОРОЖНО ЗДЕСЬ И ДАЛЕЕ ГОВНОКОД НАПИСАННЫЙ ПОД СТРЕССОМ! Я ВАС ПРЕДУПРЕДИЛ!
 """
 
-markup = types.ReplyKeyboardMarkup()
-itembtn1 = types.KeyboardButton('информация')
-markup.add(itembtn1)
+token = "f729e0d0d654a503b745610253ae910aef4af57b473998d7e0811326abdb107fe1535a825ea9a699ec002"
 
-token = "1364150425:AAErYiwGtWHTM11dL82aY6uL4rte6IwhqiM"
+bot = vk_api.VkApi(token=token)
 
-bot = telebot.TeleBot(token)
+longpoll = VkLongPoll(bot)
 
 #XLSX_URL = "https://6c869467-a-7da13995-s-sites.googlegroups.com/a/mpt.ru/priemnaa-komissia-2020/home/090207%20%20%20%D0%91%D0%AE%D0%94%D0%96%D0%95%D0%A2%20.xlsx?attachauth=ANoY7coOmUFB5Ramu3cJrEdrMzRqXq0HTbQgyRTUsY6cc-4ryf5yMUOiLqyNTpBCi_qSygzWeaXNStBDBnpzgpUEejaajauLUfKA1-WBOUse91A5CO7lx1lQ17_3HVpDbcLte1zps7i6hwSAwtd0rdnju4Z4pX7INVjAKnoEEF_yTpCL99L2owONfUNV3-oA_EaqByXfp9VsC-HmmsTOnDQ5gcFtJf5rkppU-UZmA4fQSDIXsz6rkN8wi_ZlVbXkrEvc9oyu8UCEon0tqbBDHVCuGWYnCtOdsg%3D%3D&attredirects=0&d=1"
 excel_file = xlrd.open_workbook('temp.xlsx')
@@ -23,10 +23,16 @@ excel_file = xlrd.open_workbook('temp.xlsx')
 #выбираем активный лист
 sheet = excel_file.sheet_by_index(0)
 
+def sendMessage(user_id, message):
+	bot.method("messages.send", {"user_id": user_id, "message":message, "random_id": randint(0,1024*1024)})
+
 def getExcelTable(tr):
 	global excel_file, sheet
 	
-	r = requests.get("https://sites.google.com/a/mpt.ru/priemnaa-komissia-2020/")
+	try:
+		r = requests.get("https://sites.google.com/a/mpt.ru/priemnaa-komissia-2020/")
+	except:
+		return (False, "Не удалось подключится к сайту")
 	
 	print(r.status_code)
 	#print(r.text)
@@ -34,7 +40,10 @@ def getExcelTable(tr):
 	b = BeautifulSoup(r.content, "lxml")
 	#						tr 5 (Инф безопасность)
 	#table 3 (специальности) tr 2 (Инф системы и прог) td 1 (350 мест)
-	r = requests.get(b.find_all("table")[3].find_all("tr")[tr].find_all("td")[1].find("a")["href"])
+	try:
+		r = requests.get(b.find_all("table")[3].find_all("tr")[tr].find_all("td")[1].find("a")["href"])
+	except:
+		return (False, "Не удалось получить таблицу")
 	
 	text_start = r.text.find("http://sites.google.com/a/mpt.ru/priemnaa-komissia-2020/home/")
 	
@@ -126,25 +135,26 @@ def getExcelPosition(tr):
 			"status": status
 		}
 		
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-	global getExcelTable
-	if (message.text == "/start"):
-		bot.send_message(message.from_user.id, "Жми кнопку", reply_markup=markup)
-	if (message.text) == "информация":
-		bot.send_message(message.from_user.id, "Отправил запрос на удаленный сервер... Жду файла xlsx")
-		for tr in [2,5]:
-			if tr == 2:
-				profa = "Программирование"
-			elif tr == 5:
-				profa = "Безопасность"
 
-			result = getExcelTable(tr)
-			if(not result[0]):
-				bot.send_message(message.from_user.id, result[1])
-				break # Прерываем дальнейшее выполнение цикла из-за ошибки
-			response = getExcelPosition(tr)
-			bot.send_message(message.from_user.id, profa + ": Ты на: " + response["position"] + " месте\nПозиция со схожим баллом в уведомления:" + response["same_score_position"] + " ("+ response["same_ball"] + ")" + "\nСтатус твоего уведомления:" + response["status"] + "\nВсего подано уведомлений: " + response["uvedomleni"] + "\nПоследний балл на уведомлениях: " + response["last_uvedomlenie_ball"] + "\nВсего подано заявлений:" + response["vsego"] + "\nОбщий средний балл:" + response["sredni"], reply_markup=markup)
+for event in longpoll.listen():
 
-#print(getExcelPosition()["sredni"])
-bot.polling(none_stop=True, interval=0)
+	if event.type == VkEventType.MESSAGE_NEW:
+
+		if event.to_me:
+
+			text = event.text
+
+			if (text) == "информация":
+				sendMessage(event.user_id, "Отправил запрос на удаленный сервер... Жду файла xlsx")
+				for tr in [2,5]:
+					if tr == 2:
+						profa = "Программирование"
+					elif tr == 5:
+						profa = "Безопасность"
+
+					result = getExcelTable(tr)
+					if(not result[0]):
+						sendMessage(event.user_id,  result[1])
+						break # Прерываем дальнейшее выполнение цикла из-за ошибки
+					response = getExcelPosition(tr)
+					sendMessage(event.user_id, profa + ": Ты на: " + response["position"] + " месте\nПозиция со схожим баллом в уведомления:" + response["same_score_position"] + " ("+ response["same_ball"] + ")" + "\nСтатус твоего уведомления:" + response["status"] + "\nВсего подано уведомлений: " + response["uvedomleni"] + "\nПоследний балл на уведомлениях: " + response["last_uvedomlenie_ball"] + "\nВсего подано заявлений:" + response["vsego"] + "\nОбщий средний балл:" + response["sredni"], reply_markup=markup)
